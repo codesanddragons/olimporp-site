78ed10e9fbbdb4b3cec2bd785cec9f58066eff7d
Michel S. Eckhardt <michel.desenvolvedor@gmail.com>
2022-12-12 03:34:45 -0300

    fix: baseurlfix

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-axios.defaults.baseURL = "http://localhost:3350"
+axios.defaults.baseURL = "http://177.54.147.108:3350"
 
 Vue.use({
     install(Vue){
